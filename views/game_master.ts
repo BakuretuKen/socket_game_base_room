5ec167da09972a43bf70c1b8ef9f78b3e968e053
BakuretuKen <bakuretuken7@gmail.com>
2025-08-11 14:11:19 +0900

    add gameScreen

--- views/game_master.ts
+++ views/game_master.ts
@@ -2,6 +2,11 @@
 (function() {
     window.receiveMessage = function(arr: RecvMessage): void {
         console.log("recv(master):", arr);
+        switch (arr.action) {
+            case "START_GAME":
+                document.getElementById("waitingRoom")!.style.display = "none";
+                document.getElementById("gameScreen")!.style.display = "block";
+                break;
+        }
     };
 })();
-
--- views/game_player.ts
+++ views/game_player.ts
@@ -2,5 +2,11 @@
 (function() {
     window.receiveMessage = function(arr: RecvMessage): void {
         console.log("recv(player):", arr);
+        switch (arr.action) {
+            case "START_GAME":
+                document.getElementById("waitingRoom")!.style.display = "none";
+                document.getElementById("gameScreen")!.style.display = "block";
+                break;
+        }
     };
 })();
